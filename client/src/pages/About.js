ed1f921e0812e03241d4123b4c65d57b10c3f0c6
SUPHAL-MAITY <suphalrohit@gmail.com>
2024-02-19 20:23:53 +0530

    about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,59 +1,67 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
 import "../styles/About.css";
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+
 const About = () => {
+  const handleDragStart = (e) => e.preventDefault();
+
+
+
+
+  const items = [
+    <img src="../../images/banner1.jpg" style={{height:"180px",width:"100%"}} onDragStart={handleDragStart} role="presentation" />,
+    <img src="../../images/banner2.jpg" style={{height:"180px",width:"100%"}} onDragStart={handleDragStart} role="presentation" />,
+    <img src="../../images/banner3.jpg" style={{height:"180px",width:"100%"}} onDragStart={handleDragStart} role="presentation" />,
+    
+  ];
+
+
+
   return (
     <Layout>
-      <img
-        src="/images/banner.png"
-        className="banner-img"
-        alt="bannerimage"
-        width={"100%"}
-      />
+      
+       <AliceCarousel mouseTracking items={items} />
  <div>
   <div className="about-section">
-    <h1>About Us Page</h1>
-    <p>Some text about who we are and what we do.</p>
-    <p>Resize the browser window to see that this page is responsive by the way.</p>
+    <h2>About Us </h2>
+    <p>Welcome to our e-commerce platform!</p>
+    <p>We're here to redefine your online shopping experience, offering a seamless blend of convenience, quality, and affordability.</p>
   </div>
   <h2 style={{textAlign: 'center'}}>Our Team</h2>
   <div className="row">
+   
     <div className="column">
-      <div className="card">
-        <img src="/w3images/team1.jpg" alt="Jane" style={{width: '100%'}} />
-        <div className="container">
-          <h2>Jane Doe</h2>
-          <p className="title">CEO &amp; Founder</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-          <p>jane@example.com</p>
-          <p><button className="button">Contact</button></p>
+      <div className="card" style={{display:"flex",justifyContent:"center", alignItems: "center"}} >
+        <img src="../../images/about2.jpg" alt="Mike" style={{width: '80%',height:"330px"}} />
+        
+        <div className="container" style={{display:"flex",justifyContent:"center", alignItems: "center"}} >
+          <h4>Mark </h4>     
         </div>
       </div>
     </div>
     <div className="column">
-      <div className="card">
-        <img src="/w3images/team2.jpg" alt="Mike" style={{width: '100%'}} />
-        <div className="container">
-          <h2>Mike Ross</h2>
-          <p className="title">Art Director</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-          <p>mike@example.com</p>
-          <p><button className="button">Contact</button></p>
+      <div className="card" style={{display:"flex",justifyContent:"center", alignItems: "center"}} >
+        <img src="../../images/suphal.jpg" alt="Mike" style={{width: '60%',height:"330px"}} />
+        
+        <div className="container" style={{display:"flex",justifyContent:"center", alignItems: "center"}} >
+          <h4>Suphal </h4>     
         </div>
       </div>
     </div>
     <div className="column">
-      <div className="card">
-        <img src="/w3images/team3.jpg" alt="John" style={{width: '100%'}} />
-        <div className="container">
-          <h2>John Doe</h2>
-          <p className="title">Designer</p>
-          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-          <p>john@example.com</p>
-          <p><button className="button">Contact</button></p>
+      <div className="card" style={{display:"flex",justifyContent:"center", alignItems: "center"}} >
+        <img src="../../images/about3.jpg" alt="Mike" style={{width: '80%',height:"330px"}} />
+        
+        <div className="container" style={{display:"flex",justifyContent:"center", alignItems: "center"}} >
+          <h4>Joe</h4>     
         </div>
       </div>
     </div>
+    
+    
   </div>
 </div>
 
